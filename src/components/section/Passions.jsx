16812be64f261d2d6722b955f481535d372eea6e
amzad949 <amzad.khan@ftachdata.com>
2025-02-07 21:35:24 +0530

    ui

--- src/components/section/Passions.jsx
+++ src/components/section/Passions.jsx
@@ -5,7 +5,7 @@ function Passions() {
     const { scrollYProgress } = useScroll();
 
     // Transform scroll progress into x-axis movement
-    const x = useTransform(scrollYProgress, [0, 1], ["-100%", "100%"]);
+    const x = useTransform(scrollYProgress, [0, 1], ["100%", "-100%"]);
 
     return (
         <div className="h-screen flex items-center justify-center overflow-hidden bg-[url('https://cdn.prod.website-files.com/672f0d4d7f4c51f604c1a912/672f0d4d7f4c51f604c1a92b_passions.avif')] bg-cover bg-center">
