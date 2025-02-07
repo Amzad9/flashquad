a88619a5bbe828572d84d332ca3fee258dd67cea
amzad949 <amzad.khan@ftachdata.com>
2025-02-07 15:42:37 +0530

    hero ui changes

--- src/components/section/heroSection.jsx
+++ src/components/section/heroSection.jsx
@@ -2,21 +2,23 @@
 import HeroImage from '../../assets/hero-phone.png'
 function HeroSection() {
     return (
-        <header className='pt-20 overflow-hidden h-screen'>
-            <div className="h-screen relative flex items-center justify-center">
-                <div className="flex gap-4 absolute z-0 gap-[3rem] mb-20">
-                    <div className="w-[27rem]">
-                        <h2 className="text-7xl font-normal text-center text-white">Meet </h2>
- <h2 className="text-7xl font-normal text-center  text-white">People</h2>
+        <header className='md:pt-20 pt-35 overflow-hidden h-screen relative'>
+            <div className="h-[300px] absolute bottom-0 z-20 w-full bg-gradient-to-t from-black via-black-900/100 to-transparent">
+            </div>
+            <div className="h-screen relative flex flex-col md:flex-row md:items-center justify-center">
+                <div className="flex flex-col md:flex-row gap-0 md:gap-4 sm:relative md:absolute z-0 gap-0 md:gap-[3rem] mb-5 md:mb-20">
+                    <div className="w-full md:w-[27rem] gap-3 flex md:flex-col justify-center">
+                        <h2 className="text-3xl md:text-7xl font-normal text-center text-white">Meet </h2>
+                        <h2 className="text-3xl md:text-7xl font-normal text-center  text-white">People</h2>
                     </div>
 
-                    <div className="w-[27rem]">
-                        <h2 className="text-7xl font-normal text-center  text-white">Your </h2>
-    <h2 className="text-7xl font-normal text-center text-white">Way</h2>
+                    <div className="w-full md:w-[27rem] gap-3 flex md:flex-col justify-center">
+                        <h2 className="text-3xl md:text-7xl  font-normal text-center  text-white">Your </h2>
+                        <h2 className="text-3xl md:text-7xl font-normal text-center text-white">Way</h2>
                     </div>
                 </div>
                 <div className='relative z-1 flex items-center justify-center'>
-                    <img src={HeroImage} alt="Hero" className="w-[40%]" />
+                    <img src={HeroImage} alt="Hero" className="w-[90%] md:ms-8 ms-12 md:w-[40%]" />
                 </div>
             </div>
         </header>
