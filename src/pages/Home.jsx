c1530761b631589c91fa4e94f954b6e8400fae46
amzad949 <amzad.khan@ftachdata.com>
2025-02-07 17:23:39 +0530

    discover ui changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import Discover from "../components/section/Discover"
 import HeroSection from "../components/section/HeroSection"
 
 function Home() {
     return (
         <div>
             <HeroSection />
+            <Discover />
         </div>
     )
 }
